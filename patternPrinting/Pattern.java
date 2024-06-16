f53be3a7119a87db5035a075944f9ec7b703ddb6
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-06-16 09:49:10 +0530

    Solved pattern issue

--- patternPrinting/Pattern.java
+++ patternPrinting/Pattern.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        pattern01(n);
+        int nu = sc.nextInt();
+        pattern01(nu);
         sc.close();
     }
 
     public static void pattern01(int n) {
-        for (int row = 1; row <= n; row++) { // Changed condition to row <= n
-            for (int col = 1; col <= row; col++) { // Changed condition to col <= row
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("*");
             }
             System.out.println();
